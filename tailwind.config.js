082753ba1cdcb58edd0df94147b6a20b3a3a5449
hahz21 <htetaunghlaingzin@gmail.com>
2025-02-11 19:16:00 +0630

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('/images/Group.png')",
+      }
+    },
+  },
+  plugins: [],
+}
+
